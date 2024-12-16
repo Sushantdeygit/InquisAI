19e6b10e2aba951afb3163e910b86030041283ab
Sushant <sushantdey089@gmail.com>
2024-12-16 17:20:02 +0530

    added PROD_CLIENT_URL to socket cors

--- config/globals.config.js
+++ config/globals.config.js
@@ -1,32 +1,33 @@
-import { config } from 'dotenv';
-config();
+import { config } from 'dotenv'
+config()
 
 // Server secrets
-export const PORT = process.env.PORT || 5000;
-export const DEFAULT_IMAGE_URL = process.env.DEFAULT_IMAGE_URL;
-export const BCRYPT_SALT = process.env.BCRYPT_SALT || 10;
-export const CLIENT_URL = process.env.CLIENT_URL;
-export const ENV = process.env.ENV;
+export const PORT = process.env.PORT || 5000
+export const DEFAULT_IMAGE_URL = process.env.DEFAULT_IMAGE_URL
+export const BCRYPT_SALT = process.env.BCRYPT_SALT || 10
+export const CLIENT_URL = process.env.CLIENT_URL
+export const PROD_CLIENT_URL = process.env.PROD_CLIENT_URL
+export const ENV = process.env.ENV
 
 // MongoDB keys
-export const MONGO_URI = process.env.MONGO_URI;
+export const MONGO_URI = process.env.MONGO_URI
 
 // JWT keys
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const JWT_LIFETIME = process.env.JWT_LIFETIME || '30d';
+export const JWT_SECRET = process.env.JWT_SECRET
+export const JWT_LIFETIME = process.env.JWT_LIFETIME || '30d'
 
 // Google Keys used for nodemailer
-export const GOOGLE_APP_PASSWORD = process.env.GOOGLE_APP_PASSWORD;
-export const GOOGLE_USER_ID = process.env.GOOGLE_USER_ID;
+export const GOOGLE_APP_PASSWORD = process.env.GOOGLE_APP_PASSWORD
+export const GOOGLE_USER_ID = process.env.GOOGLE_USER_ID
 
 // Open AI Credentials
-export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 
 // Deepgram Transcribe key
-export const DEEPGRAM_API_KEY = process.env.DEEPGRAM_API_KEY;
+export const DEEPGRAM_API_KEY = process.env.DEEPGRAM_API_KEY
 
 // AWS Credentials
-export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
-export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
-export const AWS_REGION = process.env.AWS_REGION;
-export const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID
+export const AWS_REGION = process.env.AWS_REGION
+export const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME
--- sockets/index.js
+++ sockets/index.js
@@ -1,26 +1,26 @@
-import { Server } from 'socket.io';
-import interviewsHandler from './interview.js';
-import { CLIENT_URL } from '../config/globals.config.js';
+import { Server } from 'socket.io'
+import interviewsHandler from './interview.js'
+import { CLIENT_URL } from '../config/globals.config.js'
+import { PROD_CLIENT_URL } from '../config/globals.config.js'
 
 export default function initializeSocket(httpServer) {
-    const io = new Server(httpServer, {
-        cors: {
-            origin: [CLIENT_URL]
-        }
-    }
-    );
+  const io = new Server(httpServer, {
+    cors: {
+      origin: [CLIENT_URL, PROD_CLIENT_URL],
+    },
+  })
 
-    // Socket.IO event handlers
-    io.on('connection', (socket) => {
-        console.log('A user connected');
+  // Socket.IO event handlers
+  io.on('connection', (socket) => {
+    console.log('A user connected')
 
-        // Handle disconnection
-        socket.on('disconnect', () => {
-            console.log('User disconnected');
-        });
-    });
+    // Handle disconnection
+    socket.on('disconnect', () => {
+      console.log('User disconnected')
+    })
+  })
 
-    // Initialize other socket event handlers
-    interviewsHandler(io);
-    // Add other socket event handlers here if needed
+  // Initialize other socket event handlers
+  interviewsHandler(io)
+  // Add other socket event handlers here if needed
 }
